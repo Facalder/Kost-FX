2b96e22116ed2577444d7161e8b7023dd93bc5f1
Facalder <faainamacaldera16@gmail.com>
2025-01-10 08:14:15 +0700

    feat(Models): Creating model of Kost and Akun

---
+++ src/main/java/com/kost/models/Akun.java
@@ -0,0 +1,72 @@
+package com.kost.models;
+
+public class Akun {
+    private String nama_pengguna;
+    private String kata_sandi;
+    private String role;
+    private String security_question;
+    private String security_answer;
+
+    public Akun(String nama_pengguna, String kata_sandi, String role, String security_question, String security_answer) {
+        this.nama_pengguna = nama_pengguna;
+        this.kata_sandi = kata_sandi;
+        this.role = role;
+        this.security_question = security_question;
+        this.security_answer = security_answer;
+    }
+
+    public Akun() {
+
+    }
+
+    public String getNama_pengguna() {
+        return nama_pengguna;
+    }
+
+    public void setNama_pengguna(String nama_pengguna) {
+        this.nama_pengguna = nama_pengguna;
+    }
+
+    public String getKata_sandi() {
+        return kata_sandi;
+    }
+
+    public void setKata_sandi(String kata_sandi) {
+        this.kata_sandi = kata_sandi;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getSecurity_question() {
+        return security_question;
+    }
+
+    public void setSecurity_question(String security_question) {
+        this.security_question = security_question;
+    }
+
+    public String getSecurity_answer() {
+        return security_answer;
+    }
+
+    public void setSecurity_answer(String security_answer) {
+        this.security_answer = security_answer;
+    }
+
+    @Override
+    public String toString() {
+        return "Akun{" +
+                "nama_pengguna='" + nama_pengguna + '\'' +
+                ", kata_sandi='" + kata_sandi + '\'' +
+                ", role='" + role + '\'' +
+                ", security_question='" + security_question + '\'' +
+                ", security_answer='" + security_answer + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/kost/models/Kost.java
@@ -0,0 +1,79 @@
+package com.kost.models;
+
+public class Kost {
+    private String alamat;
+    private String fasilitas;
+    private double harga;
+    private int total_kamar;
+    private int kamar_tersedia;
+    private String foto;
+
+    public Kost(String alamat, String fasilitas, double harga, int total_kamar, int kamar_tersedia, String foto) {
+        this.alamat = alamat;
+        this.fasilitas = fasilitas;
+        this.harga = harga;
+        this.total_kamar = total_kamar;
+        this.kamar_tersedia = kamar_tersedia;
+        this.foto = foto;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getFasilitas() {
+        return fasilitas;
+    }
+
+    public void setFasilitas(String fasilitas) {
+        this.fasilitas = fasilitas;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public int getTotal_kamar() {
+        return total_kamar;
+    }
+
+    public void setTotal_kamar(int total_kamar) {
+        this.total_kamar = total_kamar;
+    }
+
+    public int getKamar_tersedia() {
+        return kamar_tersedia;
+    }
+
+    public void setKamar_tersedia(int kamar_tersedia) {
+        this.kamar_tersedia = kamar_tersedia;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    @Override
+    public String toString() {
+        return "Kost{" +
+                "alamat='" + alamat + '\'' +
+                ", fasilitas='" + fasilitas + '\'' +
+                ", harga=" + harga +
+                ", total_kamar=" + total_kamar +
+                ", kamar_tersedia=" + kamar_tersedia +
+                ", foto='" + foto + '\'' +
+                '}';
+    }
+}
