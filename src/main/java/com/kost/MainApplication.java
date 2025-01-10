43de67a59ccca0c04b84f65c859970a5f0557716
Facalder <faainamacaldera16@gmail.com>
2025-01-10 08:08:59 +0700

    feat(Init Project): Initializing and scaffholding the project

---
+++ src/main/java/com/kost/MainApplication.java
@@ -0,0 +1,51 @@
+package com.kost;
+
+import com.kost.views.AppViewManager;
+import com.gluonhq.charm.glisten.application.AppManager;
+import com.gluonhq.charm.glisten.visual.Swatch;
+import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class MainApplication extends Application {
+
+    private final AppManager appManager = AppManager.initialize(this::postInit);
+    private final Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+
+    @Override
+    public void init() {
+        AppViewManager.registerViewsAndDrawer();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        double getScreenWidth = screenBounds.getWidth();
+        double getScreenHeight = screenBounds.getHeight();
+
+        primaryStage.setMinWidth(getScreenWidth * 0.7);
+
+        primaryStage.setMaxHeight(getScreenHeight);
+        primaryStage.setMinHeight(getScreenHeight);
+
+        primaryStage.centerOnScreen();
+        primaryStage.initStyle(StageStyle.DECORATED);
+        primaryStage.setTitle("Kost Management");
+
+        appManager.start(primaryStage);
+    }
+
+    private void postInit(Scene scene) {
+        Swatch.BLUE.assignTo(scene);
+
+        scene.getStylesheets().add(MainApplication.class.getResource("style.css").toExternalForm());
+        ((Stage) scene.getWindow()).getIcons().add(new Image(MainApplication.class.getResourceAsStream("/icon.png")));
+    }
+
+    public static void main(String args[]) {
+        launch(args);
+    }
+}
---
+++ src/main/java/com/kost/views/RegisterPresenter.java
@@ -0,0 +1,35 @@
+package com.kost.views;
+
+import com.gluonhq.charm.glisten.animation.BounceInRightTransition;
+import com.gluonhq.charm.glisten.application.AppManager;
+import com.gluonhq.charm.glisten.control.AppBar;
+import com.gluonhq.charm.glisten.control.FloatingActionButton;
+import com.gluonhq.charm.glisten.mvc.View;
+import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
+import javafx.fxml.FXML;
+
+public class RegisterPresenter {
+
+    @FXML
+    private View register;
+
+    public void initialize() {
+        register.setShowTransitionFactory(BounceInRightTransition::new);
+        
+        FloatingActionButton fab = new FloatingActionButton(MaterialDesignIcon.INFO.text,
+                e -> System.out.println("Info"));
+        fab.showOn(register);
+        
+        register.showingProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue) {
+                AppManager appManager = AppManager.getInstance();
+                AppBar appBar = appManager.getAppBar();
+                appBar.setNavIcon(MaterialDesignIcon.MENU.button(e ->
+                        appManager.getDrawer().open()));
+                appBar.setTitleText("Register");
+                appBar.getActionItems().add(MaterialDesignIcon.FAVORITE.button(e -> 
+                        System.out.println("Favorite")));
+            }
+        });
+    }
+}
